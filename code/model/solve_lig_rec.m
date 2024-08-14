9811ebb4585002cff8d7044b23b94dc0fdf497bb
Yunjeong Lee <ylee14@uw.edu>
2024-08-14 15:25:17 -0700

    find indices of free ligand concentration and use it and `sol` to add the free ligand concentration to `result`

--- code/model/solve_lig_rec.m
+++ code/model/solve_lig_rec.m
@@ -13,4 +13,21 @@ for i = 1:length(y0)
     result.(params_struct.species_names{i}) = sol(:, i);
 end
 
+%% Free ligand (pM)
+% Find index for free ligands
+ind_VA = strcmp(params_struct.species_names, 'VA');
+ind_VB = strcmp(params_struct.species_names, 'VB');
+ind_Pl = strcmp(params_struct.species_names, 'Pl');
+ind_PDAA = strcmp(params_struct.species_names, 'PDAA');
+ind_PDAB = strcmp(params_struct.species_names, 'PDAB');
+ind_PDBB = strcmp(params_struct.species_names, 'PDBB');
+
+% Record the concentration
+result.VA_free = sol(:, ind_VA);
+result.VB_free = sol(:, ind_VB);
+result.Pl_free = sol(:, ind_Pl);
+result.PDAA_free = sol(:, ind_PDAA);
+result.PDAB_free = sol(:, ind_PDAB);
+result.PDBB_free = sol(:, ind_PDBB);
+
 end
