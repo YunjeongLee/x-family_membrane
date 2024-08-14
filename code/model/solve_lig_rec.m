ae14ffcee489c6747feee6ec4c542b6a695c2e59
Yunjeong Lee <ylee14@uw.edu>
2024-08-14 10:31:31 -0700

    initial commit

---
+++ code/model/solve_lig_rec.m
@@ -0,0 +1,11 @@
+function sol = solve_lig_rec(time_stamp, params_struct)
+%% Define initial condition
+params_cell = struct2cell(params_struct);
+y0 = cell2mat(params_cell(1:params_struct.num_state));
+
+%% Solve ODE system
+options = odeset('AbsTol', 1e-20, 'RelTol', 1e-9);
+
+[~, sol] = ode15s(@(t, y) ODE_lig_rec(t, y, params_struct), time_stamp, y0, options);
+
+end
