2621836f3b9b5f07822e964853649aa8084c229e
YJLee <ylee14@uw.edu>
2024-08-20 19:42:01 -0700

    add a vertical line showing the default value of receptor density

--- code/visualize/visualize_stack_area.m
+++ code/visualize/visualize_stack_area.m
@@ -2,8 +2,11 @@ function [] = visualize_stack_area(data, default, xrange, color, ttl, xtick, xla
 figure('Position', [10 10 1600 500]);
 for i = 1:size(data, 3)
     subplot(2, 3, i);
+    hold on;
     area(xrange, data(:, :, i), 'EdgeColor', 'none');
     colororder(color);
+    xline(default, 'LineWidth', 3, 'LineStyle', ':');
+    hold off;
     xlabel(sprintf('[%s] (rec/cell)', xlabels))
     ylabel('Proportion (%)')
     ylim([0 100])
