9af5929a7643dd6ca3c15d96967157ec12d24d03
Yunjeong Lee <ylee14@uw.edu>
2024-08-26 10:42:49 -0700

    initial commit

---
+++ code/visualize/visualize_free_lig.m
@@ -0,0 +1,18 @@
+function [] = visualize_free_lig(data, default, xrange, color, ttl, xtick, xlabels, lgd, filename)
+figure('Position', [10 10 1600 500]);
+for i = 1:size(data, 2)
+    subplot(2, 3, i);
+    hold on;
+    area(xrange, data(:, i), 'EdgeColor', 'none', 'facecolor', color{i});
+    colororder(color{i});
+    xline(default, 'LineWidth', 3, 'LineStyle', ':');
+    hold off;
+    xlabel(sprintf('[%s] (rec/cell)', xlabels))
+    ylabel('pM')
+    xtickformat('%,g')
+    title(ttl{i})
+    legend(lgd, 'Location', 'northeastoutside')
+    set(gca, 'fontsize', 15, 'xtick', xtick)
+end
+saveas(gca, filename, 'epsc')
+saveas(gca, filename, 'png')
