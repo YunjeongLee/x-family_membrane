5981aed5a592566124bf183b1859dacc89cc2e3f
YJLee <ylee14@uw.edu>
2024-09-09 16:30:58 -0400

    edit figure size

--- code/main_mg.m
+++ code/main_mg.m
@@ -87,7 +87,7 @@ interest = 1:find(time_stamp == 3600*2400);
 
 
 color_lig = {'#ff595e', '#ffca3a'};
-figure('Position', [10 10 1600 500])
+figure('Position', [10 10 800 400])
 a = area(time_stamp(interest)/3600, R1_dist(interest, :), 'EdgeColor', 'none');
 a(1).FaceColor = [255, 89, 95]/256;
 a(2).FaceColor = [255, 202, 58]/256;
