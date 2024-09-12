3c77875676d650d634f3ce03b58230ba38631a40
YJLee <ylee14@uw.edu>
2024-09-12 10:13:59 -0400

    plot association and dissociation rates of PlGF in two subplots

--- code/visualize/visualize_assoc_dissoc.m
+++ code/visualize/visualize_assoc_dissoc.m
@@ -1,16 +1,19 @@
 function [] = visualize_assoc_dissoc(data, default, xrange, color, ttl, xtick, xlabels, lgd, filenames)
 %% Plot dynamics of association and dissociation
-figure('Position', [10 10 900 400]);
-hold on;
-area(xrange, - data{1} + data{2}, 'EdgeColor', 'none')
-colororder(color)
-xline(default, 'LineWidth', 3, 'LineStyle', ':');
-hold off;
-xlabel(sprintf('[%s] (rec/cell)', xlabels))
-ylabel('M/s')
-title('- Association + dissociation')
-legend(lgd, 'Location', 'northeastoutside')
-set(gca, 'fontsize', 25);
+figure('Position', [10 10 1200 400]);
+for i = 1:2
+    subplot(1, 2, i)
+    hold on;
+    area(xrange, data{i}, 'EdgeColor', 'none')
+    colororder(color)
+    xline(default, 'LineWidth', 3, 'LineStyle', ':');
+    hold off;
+    xlabel(sprintf('[%s] (rec/cell)', xlabels))
+    ylabel('M/s')
+    title(ttl{i})
+    legend(lgd, 'Location', 'northeastoutside')
+    set(gca, 'fontsize', 15);
+end
 saveas(gca, filenames{1}, 'epsc')
 saveas(gca, filenames{1}, 'png')
 
