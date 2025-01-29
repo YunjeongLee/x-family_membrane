1610b95032e5638141f933789fd5b903d274b72c
Yunjeong Lee <ylee14@uw.edu>
2025-01-29 11:05:52 -0800

    delete unused function

--- code_R/etc/assign_params.R
+++
@@ -1,7 +0,0 @@
-assign_params <- function(params) {
-  for (i in 1:nrow(params)) {
-    name <- params$Parameter[i]
-    value <- params$value[i]
-    assign(x=name, value=value, envir = .GlobalEnv)
-  }
-}
