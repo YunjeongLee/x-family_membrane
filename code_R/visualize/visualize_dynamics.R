dbdeeccf9ee6cb20eb96bad7087b03ef1b19b91c
Yunjeong Lee <ylee14@uw.edu>
2025-01-29 12:09:45 -0800

    fix variable name `color_lig` -> `color_rec`

--- code_R/visualize/visualize_dynamics.R
+++ code_R/visualize/visualize_dynamics.R
@@ -63,7 +63,7 @@ visualize_dynamics <- function(time_stamp, results, result_foldername) {
   df$variable <- factor(df$variable, levels=df_colnames)
   
   # Plot free vs. bound receptor --------------------------------------------
-  df$colors = rep(rep(color_lig, each=2), length(results$time))
+  df$colors = rep(rep(color_rec, each=2), length(results$time))
   df$linestyle = rep(c("solid", "dotted"), length(results$time) * length(color_rec))
   p = ggplot(data = df, aes(x = time/3600, y = value, color = variable, linetype = variable)) +
     geom_line(linewidth = 1) +
