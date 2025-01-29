22e870b1535d8334cbee67150a848cfe27e9b5aa
Yunjeong Lee <ylee14@uw.edu>
2025-01-29 12:10:06 -0800

    fix figure filename `*_lig.pdf` -> `*_rec.pdf`

--- code_R/visualize/visualize_dynamics.R
+++ code_R/visualize/visualize_dynamics.R
@@ -73,6 +73,6 @@ visualize_dynamics <- function(time_stamp, results, result_foldername) {
     theme(text = element_text(size= 10)) +
     scale_y_continuous(breaks = seq(0, 1200, 200), labels = seq(0, 1200, 200)) + 
     theme_light()
-  ggsave(sprintf("%s/dynamics_free_vs_bound_lig.pdf", result_foldername), width = 6, height = 3, units = "in")
+  ggsave(sprintf("%s/dynamics_free_vs_bound_rec.pdf", result_foldername), width = 6, height = 3, units = "in")
   
 }
