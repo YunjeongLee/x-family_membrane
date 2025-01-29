e0320e09273e1a2c7f1b811833c9b9959b1db592
Yunjeong Lee <ylee14@uw.edu>
2025-01-29 12:06:40 -0800

    fix variable name `lig_result_name` -> `rec_result_name`

--- code_R/visualize/visualize_dynamics.R
+++ code_R/visualize/visualize_dynamics.R
@@ -49,11 +49,11 @@ visualize_dynamics <- function(time_stamp, results, result_foldername) {
     lig_result_name = lig_result[i]
     # Free ligand
     colName = paste(lig_name, '(Free)')
-    colName_results = paste0(lig_result_name, '_free')
+    colName_results = paste0(rec_result_name, '_free')
     df[[colName]] <- results[[colName_results]] * 1e12
     # Bound ligand
     colName = paste(lig_name, '(Bound)')
-    colName_results = paste0(lig_result_name, '_bound')
+    colName_results = paste0(rec_result_name, '_bound')
     df[[colName]] <- results[[colName_results]] * 1e12
   }
   df_colnames = colnames(df)
