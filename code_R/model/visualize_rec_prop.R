446035acffcd953b96cedd533b491b0c5b6ddbf4
Yunjeong Lee <ylee14@uw.edu>
2025-01-29 16:07:20 -0800

    calculate the percentage of ligand occupying each receptor

--- code_R/model/visualize_rec_prop.R
+++ code_R/model/visualize_rec_prop.R
@@ -47,4 +47,38 @@ visualize_rec_prop <- function(results, result_foldername) {
     theme(text = element_text(size= 10)) + theme_light()
   show(p)
   ggsave(sprintf("%s/Receptor_free_vs_bound.pdf", result_foldername), width = 6, height = 3, units = "in")
+  
+  # Calculate the percentage of ligand occupying each receptor --------------
+  lig = c('VEGF-A', 'VEGF-B', 'PlGF', 'PDGF-AA', 'PDGF-AB', 'PDGF-BB')
+  species <- colnames(results)
+  
+  # Initialize dataframe
+  df <- data.frame(matrix(nrow=0, ncol=3))
+  for (i in 1:length(rec_result)) {
+    rec_name = rec[i]
+    ind_VA   = which(str_detect(species, "VA_") & str_detect(species, paste0('_', rec_result[i])))
+    ind_VB   = which(str_detect(species, "VB_") & str_detect(species, paste0('_', rec_result[i])))
+    ind_Pl   = which(str_detect(species, "Pl_") & str_detect(species, paste0('_', rec_result[i])))
+    ind_PDAA = which(str_detect(species, "PDAA_") & str_detect(species, paste0('_', rec_result[i])))
+    ind_PDAB = which(str_detect(species, "PDAB_") & str_detect(species, paste0('_', rec_result[i])))
+    ind_PDBB = which(str_detect(species, "PDBB_") & str_detect(species, paste0('_', rec_result[i])))
+    value <- c(
+      ifelse(length(ind_VA)   > 0, sum(tail(results[ind_VA],   1)), NA),
+      ifelse(length(ind_VB)   > 0, sum(tail(results[ind_VB],   1)), NA),
+      ifelse(length(ind_Pl)   > 0, sum(tail(results[ind_Pl],   1)), NA),
+      ifelse(length(ind_PDAA) > 0, sum(tail(results[ind_PDAA], 1)), NA),
+      ifelse(length(ind_PDAB) > 0, sum(tail(results[ind_PDAB], 1)), NA),
+      ifelse(length(ind_PDBB) > 0, sum(tail(results[ind_PDBB], 1)), NA)
+    )
+    value <- value/sum(value, na.rm = TRUE)
+    new_df <- data.frame(Receptor=rep(rec_name, length(color_lig)),
+                         Ligand = lig, Value=as.vector(value),
+                         row.names = NULL)
+    # Append df
+    df <- rbind(df, new_df)
+  }
+  # Fix the order of row
+  df$Receptor <- factor(df$Receptor, levels=rec)
+  df$Ligand <- factor(df$Ligand, levels=lig)
+  
 }
